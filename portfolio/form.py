dfeb3276d1a2a4ffcf0e831f431fe795d774b9ca
Tohir <tohirerkinov181@gmail.com>
2024-04-08 19:24:55 +0500

    first

---
+++ portfolio/form.py
@@ -0,0 +1,15 @@
+from django.forms import ModelForm, TextInput
+from portfolio.models import Contact
+
+
+class ContactForm(ModelForm):
+    class Meta:
+        model = Contact
+        fields = 'all'
+        fields = {
+            'phone_number': TextInput(
+                attrs={
+                    'placeholder': 'Phone number must be entered in the format: +123456789. Up to 15 figits'
+        }
+            )
+        }
---
+++ portfolio/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from .models import Project
+from .form import ContactForm
+
+
+# Create your views here.
+def home_page(request):
+
+    if request.method == 'POST':
+         form = ContactForm(request.POST)
+         if form.is_valid():
+            form.save()
+    else:
+         form = ContactForm()
+
+    projects = Project.objects.all()
+    context = {
+        'projects': projects,
+        'form': form
+    }
+    return render(request, template_name='index.html', context=context)
